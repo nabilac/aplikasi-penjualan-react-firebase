52b02e9a96594383e97aaa6397bda12949b8e5af
nabilac <nabila.carolina@quadras.co.id>
2021-02-04 17:35:55 +0700

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD72bVJiOMFY6r1-PDKhJ7eAyQwUXR8CMQ",
+    authDomain: "aplikasi-penjualan-66855.firebaseapp.com",
+    databaseURL: "https://aplikasi-penjualan-66855.firebaseio.com",
+    projectId: "aplikasi-penjualan-66855",
+    storageBucket: "aplikasi-penjualan-66855.appspot.com",
+    messagingSenderId: "941783217030",
+    appId: "1:941783217030:web:5336bdb3f4e744a450f207",
+    measurementId: "G-0JY8DDMESG"
+};
+
+export default firebaseConfig;
